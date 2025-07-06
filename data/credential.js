87c36291e3c70c6ebf7fb6d53b943c671dde312a
yogap02 <yogalaxy69@gmail.com>
2025-07-06 21:14:01 +0700

    test(Sorting Checkout) : add additional test

--- data/credential.js
+++ data/credential.js
@@ -1,9 +1,9 @@
 export const credential = {
-  username: 'standard_user',
-  password: 'secret_sauce',
-  firstName: 'tom',
-  lastName: 'bob',
-  postalCode: '12345',
-  tax: 0.08,
-  desiredItems: ['Sauce Labs Bolt T-Shirt', 'Sauce Labs Fleece Jacket', 'Sauce Labs Onesie'],
+  username: '',
+  password: '',
+  firstName: '',
+  lastName: '',
+  postalCode: '',
+  tax: 0,
+  desiredItems: [],
 };
